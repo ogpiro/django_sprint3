ac7bb366c4d4377942b3e1409f59163f897abd6f
Владимир Миняйлов <ya.zamazka@yandex.ru>
2023-06-23 16:17:11 +0300

    Итерация 1 (пеп8)

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -21,12 +21,11 @@ class Location(PublishedModel):
 class Category(PublishedModel):
     title = models.CharField(max_length=256, verbose_name='Заголовок')
     description = models.TextField(verbose_name='Описание')
-    slug = models.SlugField(
-        unique=True,
-        verbose_name='Идентификатор',
-        help_text='Идентификатор страницы для URL; '
-        'разрешены символы латиницы, цифры, дефис и подчёркивание.'
-        )
+    slug = models.SlugField(unique=True,
+                            verbose_name='Идентификатор',
+                            help_text='Идентификатор страницы для URL; '
+                            'разрешены символы латиницы, цифры, дефис '
+                            'и подчёркивание.')
 
     class Meta:
         verbose_name = 'категория'
@@ -39,11 +38,10 @@ class Category(PublishedModel):
 class Post(PublishedModel):
     title = models.CharField(max_length=256, verbose_name='Заголовок')
     text = models.TextField(verbose_name='Текст')
-    pub_date = models.DateTimeField(
-        verbose_name='Дата и время публикации',
-        help_text='Если установить дату и время в будущем — '
-        'можно делать отложенные публикации.'
-        )
+    pub_date = models.DateTimeField(verbose_name='Дата и время публикации',
+                                    help_text='Если установить дату и время в '
+                                    'будущем — можно делать отложенные '
+                                    'публикации.')
     author = models.ForeignKey(User,
                                on_delete=models.CASCADE,
                                verbose_name='Автор публикации')
--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -5,39 +5,34 @@ from .models import Category, Post
 
 def index(request):
     current_time = timezone.now()
-    post_list = Post.objects.filter(
-        pub_date__lte=current_time,
-        is_published=True,
-        category__is_published=True
-    ).order_by('-pub_date')[:5]
+    post_list = Post.objects.filter(pub_date__lte=current_time,
+                                    is_published=True,
+                                    category__is_published=True
+                                    ).order_by('-pub_date')[:5]
     context = {'post_list': post_list}
     return render(request, 'blog/index.html', context)
 
 
 def post_detail(request, id):
     current_time = timezone.now()
-    post = get_object_or_404(
-        Post,
-        id=id,
-        pub_date__lte=current_time,
-        is_published=True,
-        category__is_published=True
-        )
+    post = get_object_or_404(Post,
+                             id=id,
+                             pub_date__lte=current_time,
+                             is_published=True,
+                             category__is_published=True)
     context = {'post': post}
     return render(request, 'blog/detail.html', context)
 
 
 def category_posts(request, category_slug):
     current_time = timezone.now()
-    category = get_object_or_404(
-        Category,
-        slug=category_slug,
-        is_published=True
-        )
-    post_list = Post.objects.filter(
-        category=category,
-        pub_date__lte=current_time,
-        is_published=True
-    )
+    category = get_object_or_404(Category,
+                                 slug=category_slug,
+                                 is_published=True
+                                 )
+    post_list = Post.objects.filter(category=category,
+                                    pub_date__lte=current_time,
+                                    is_published=True
+                                    )
     context = {'category': category, 'post_list': post_list}
     return render(request, 'blog/category.html', context)
