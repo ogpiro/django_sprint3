e58371b95645f1013c7a11c6ee3285ef9d2540af
Владимир Миняйлов <ya.zamazka@yandex.ru>
2023-06-23 15:51:22 +0300

    Итерация 1

---
+++ blogicum/blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from blog.views import index, post_detail, category_posts
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('posts/<int:id>/', post_detail, name='post_detail'),
+    path('category/<slug:category_slug>/', category_posts,
+         name='category_posts'),
+]
